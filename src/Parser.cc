7843191d587118d29c3d2d95a0cd4bd51e6518d9
Zdenek Nemec <zdenek@me.com>
2013-05-18 18:51:24 +0200

    Handling underlaying parser exceptions.

--- src/Parser.cc
+++ src/Parser.cc
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Apiary.io. All rights reserved.
 //
 
+#include <exception>
+#include <sstream>
 #include "Parser.h"
 #include "MarkdownParser.h"
 #include "BlueprintParser.h"
@@ -16,14 +18,26 @@ const int SourceAnnotation::OK;
 
 void Parser::parse(const SourceData& source, Result& result, Blueprint& blueprint)
 {
-    // Parse Markdown
-    MarkdownBlock::Stack markdown;
-    MarkdownParser markdownParser;
-    markdownParser.parse(source, result, markdown);
-    
-    if (result.error.code != Error::OK)
-        return;
-    
-    // Parse Blueprint
-    BlueprintParser::Parse(source, markdown, result, blueprint);
+    try {
+        // Parse Markdown
+        MarkdownBlock::Stack markdown;
+        MarkdownParser markdownParser;
+        markdownParser.parse(source, result, markdown);
+        
+        if (result.error.code != Error::OK)
+            return;
+        
+        // Parse Blueprint
+        BlueprintParser::Parse(source, markdown, result, blueprint);
+    }
+    catch (const std::exception& e) {
+
+        std::stringstream ss;
+        ss << "parser exception: '" << e.what() << "'";
+        result.error = Error(ss.str(), 1);
+    }
+    catch (...) {
+        
+        result.error = Error("parser exception has occured", 1);
+    }
 }
