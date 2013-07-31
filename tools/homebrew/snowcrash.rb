a40614ac14e173e1683a51fc255ab0c8ea887ca1
Z <zdenek@me.com>
2013-07-31 08:04:39 -0400

    remove python dependency from the brew recipe

--- tools/homebrew/snowcrash.rb
+++ tools/homebrew/snowcrash.rb
@@ -3,7 +3,6 @@ require 'formula'
 class Snowcrash < Formula
   homepage 'http://apiblueprint.org'
   head 'https://github.com/apiaryio/snowcrash.git', :tag => 'v0.2.3'
-  depends_on :python
 
   def install
     system "./configure"
