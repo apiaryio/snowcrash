1592cd66c58e6c0648d15a2809d11bc1b05e06bc
Z <zdenek@me.com>
2013-12-05 15:28:50 +0100

    Prepare for v0.7.2 release

--- tools/homebrew/snowcrash.rb
+++ tools/homebrew/snowcrash.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Snowcrash < Formula
   homepage 'http://apiblueprint.org'
-  head 'https://github.com/apiaryio/snowcrash.git', :tag => 'v0.7.1'
+  head 'https://github.com/apiaryio/snowcrash.git', :tag => 'v0.7.2'
 
   def install
     system "./configure"
