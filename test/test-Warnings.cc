e121bcc03ecb368f12a89b6e9cbb459c6978c6a6
kuangmarkeleven <kuangmarkeleven@gmail.com>
2014-03-24 09:37:03 +0000

    issue #78
    uri template parsing and validation

--- test/test-Warnings.cc
+++ test/test-Warnings.cc
@@ -44,17 +44,54 @@ TEST_CASE("Warn about brackets in URI template", "[bracketwarnings][issue][#79]"
 {
     // Blueprint in question:
     //R"(
-    //FORMAT: X-1A
+    //"FORMAT: 1A\n"\
     //
-    //# GROUP test
-    //## GET /test/{id}[5]
+    //# test api
+    //this is a description of the test api
+    //# Group test
+    //A test group
+    //## test [/test/{id}[2]]
+    //"A test uri template
     //");
     const std::string blueprintSource = \
-        "HOST http://test.test.com\n"\
         "FORMAT: 1A\n"\
         "\n"\
-        "# GET /test/{id}[5]\n"\
-        "+ Response 200\n";
+        "# test api\n"\
+        "this is a description of the test api\n"\
+        "# Group test\n"\
+        "A test group\n"\
+        "## test [/test/{id}[2]]\n"\
+        "A test uri template\n";
+
+    Parser parser;
+    Result result;
+    Blueprint blueprint;
+    parser.parse(blueprintSource, 0, result, blueprint);
+    REQUIRE(result.warnings.size() == 1);
+}
+
+TEST_CASE("Warn about unsupported uri template label expansion", "[unsupportedlabelexpansion][issue][#78]")
+{
+    // Blueprint in question:
+    //R"(
+    //"FORMAT: 1A\n"\
+        //
+    //# test api
+    //this is a description of the test api
+    //# Group test
+    //A test group
+    //## test [/test/{.varone}]
+    //"A test uri template
+    //");
+    const std::string blueprintSource = \
+        "FORMAT: 1A\n"\
+        "\n"\
+        "# test api\n"\
+        "this is a description of the test api\n"\
+        "# Group test\n"\
+        "A test group\n"\
+        "## test [/test/{.varone}]\n"\
+        "A test uri template\n";
 
     Parser parser;
     Result result;
