052c44ff841ba67f69b48f5a683a7b5368728482
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-06-04 17:42:11 +0530

    Finish HeadersParser refactored tests

--- src/HeadersParser.h
+++ src/HeadersParser.h
@@ -36,7 +36,7 @@ namespace snowcrash {
             mdp::ByteBuffer content;
             CodeBlockUtility::signatureContentAsCodeBlock(node, pd, report, content);
 
-            headersFromContent(node, pd, report, out, content);
+            headersFromContent(node, content, pd, report, out);
 
             return ++MarkdownNodeIterator(node);
         }
@@ -55,7 +55,7 @@ namespace snowcrash {
             mdp::ByteBuffer content;
             CodeBlockUtility::contentAsCodeBlock(node, pd, report, content);
 
-            headersFromContent(node, pd, report, out, content);
+            headersFromContent(node, content, pd, report, out);
 
             return ++MarkdownNodeIterator(node);
         }
@@ -85,16 +85,16 @@ namespace snowcrash {
 
         /** Retrieve headers from content */
         static void headersFromContent(const MarkdownNodeIterator& node,
+                                       const mdp::ByteBuffer& content,
                                        SectionParserData& pd,
                                        Report& report,
-                                       Headers& headers,
-                                       mdp::ByteBuffer content) {
+                                       Headers& headers) {
             std::vector<std::string> lines = Split(content, '\n');
 
             for (std::vector<std::string>::iterator line = lines.begin();
                  line != lines.end();
                  ++line) {
-                if (TrimString(*line) == "") {
+                if (TrimString(*line).empty()) {
                     continue;
                 }
 
@@ -102,6 +102,7 @@ namespace snowcrash {
 
                 if (CodeBlockUtility::keyValueFromLine(*line, header)) {
                     if (findHeader(headers, header) != headers.end()) {
+                        // WARN: duplicate header on this level
                         std::stringstream ss;
 
                         ss << "duplicate definition of '" << header.first << "' header";
@@ -114,6 +115,7 @@ namespace snowcrash {
 
                     headers.push_back(header);
                 } else {
+                    // WARN: unable to parse header
                     mdp::CharactersRangeSet sourceMap = mdp::BytesRangeSetToCharactersRangeSet(node->sourceMap, pd.sourceData);
                     report.warnings.push_back(Warning("unable to parse HTTP header, expected '<header name> : <header value>', one header per line",
                                                       FormattingWarning,
@@ -134,78 +136,5 @@ namespace snowcrash {
     /** Headers Section Parser */
     typedef SectionParser<Headers, ListSectionAdapter> HeadersParser;
 }
-
-//namespace snowcrash {
-//    
-//    /** Helper for handling parsing of deprecated header sections */
-//    template <typename T>
-//    ParseSectionResult HandleDeprecatedHeaders(const BlueprintSection& section,
-//                                               const BlockIterator& cur,
-//                                               BlueprintParserCore& parser,
-//                                               T& t) {
-//        
-//        ParseSectionResult result = HandleHeaders<T>(section, cur, parser, t);
-//        
-//        // WARN: Deprecated header sections
-//        std::stringstream ss;
-//        ss << "the 'headers' section at this level is deprecated and will be removed in a future, use respective payload header section(s) instead";
-//        BlockIterator nameBlock = ListItemNameBlock(cur, section.bounds.second);
-//        SourceCharactersBlock sourceBlock = CharacterMapForBlock(nameBlock, cur, section.bounds, parser.sourceData);
-//        result.first.warnings.push_back(Warning(ss.str(),
-//                                                DeprecatedWarning,
-//                                                sourceBlock));
-//        return result;
-//    }
-//    
-//    /** \brief Copy headers into example paylods headers. */
-//    FORCEINLINE void InjectDeprecatedHeaders(const Collection<Header>::type& headers,
-//                                        Collection<TransactionExample>::type& examples)
-//    {
-//        for (Collection<TransactionExample>::iterator exampleIt = examples.begin();
-//             exampleIt != examples.end();
-//             ++exampleIt) {
-//            
-//            // Requests
-//            for (Collection<Request>::iterator reqIt = exampleIt->requests.begin();
-//                 reqIt != exampleIt->requests.end();
-//                 ++reqIt) {
-//                
-//                reqIt->headers.insert(reqIt->headers.begin(),
-//                                      headers.begin(),
-//                                      headers.end());
-//            }
-//            
-//            // Responses
-//            for (Collection<Response>::iterator resIt = exampleIt->responses.begin();
-//                 resIt != exampleIt->responses.end();
-//                 ++resIt) {
-//                
-//                resIt->headers.insert(resIt->headers.begin(),
-//                                      headers.begin(),
-//                                      headers.end());
-//            }
-//        }
-//    }
-//    
-//    // Checks T's headers for occurence of R's headers, warns if a match is found.
-//    template <class T, class R>
-//    void CheckHeaderDuplicates(const T& left,
-//                               const R& right,
-//                               const SourceDataBlock& rightSourceMap,
-//                               const SourceData& sourceData,
-//                               Result& result) {
-//        
-//        for (HeaderIterator it = right.headers.begin(); it != right.headers.end(); ++it) {
-//            if (FindHeader(left.headers, *it) != left.headers.end()) {
-//                // WARN: overshadowing header definition
-//                std::stringstream ss;
-//                ss << "overshadowing previous '" << it->first << "' header definition";
-//                result.warnings.push_back(Warning(ss.str(),
-//                                                  RedefinitionWarning,
-//                                                  MapSourceDataBlock(rightSourceMap, sourceData)));
-//            }
-//        }
-//    }
-//}
 
 #endif
