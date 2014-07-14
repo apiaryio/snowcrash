450fc48444d52ecff1cd97ee5f05149ec1d657a1
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-07-14 22:08:05 +0530

    Finished indentation, symbol and warning unit tests

--- test/test-SymbolTable.cc
+++ test/test-SymbolTable.cc
@@ -6,35 +6,31 @@
 //  Copyright (c) 2013 Apiary Inc. All rights reserved.
 //
 
-#include <iterator>
-#include "catch.hpp"
+#include "snowcrash.h"
+#include "snowcrashtest.h"
 #include "ResourceParser.h"
-#include "ResourceGroupParser.h"
-#include "Fixture.h"
 
 using namespace snowcrash;
 using namespace snowcrashtest;
 
-TEST_CASE("symbol/resource-object", "Parse object resource symbol")
+TEST_CASE("Parse object resource symbol", "[symbol_table]")
 {
-    MarkdownBlock::Stack markdown = CanonicalResourceFixture();
-    Resource resource;
-    BlueprintParserCore parser(0, SourceDataFixture, Blueprint());
-    BlueprintSection rootSection(std::make_pair(markdown.begin(), markdown.end()));
-    ParseSectionResult result = ResourceParser::Parse(markdown.begin(), markdown.end(), rootSection, parser, resource);
-    
-    REQUIRE(result.first.error.code == Error::OK);
-    
-    REQUIRE(parser.symbolTable.resourceModels.size() == 1);
-    ResourceModelSymbolTable::iterator it = parser.symbolTable.resourceModels.find("My Resource");
-    REQUIRE(it != parser.symbolTable.resourceModels.end());
-    REQUIRE(it->first == "My Resource");
-    REQUIRE(it->second.body == "X.O.");
-    
+    mdp::ByteBuffer source = \
+    "# /resource\n"\
+    "+ Super Model (text/plain)\n\n"\
+    "          {...}\n";
+
     // Check we will get error parsing the same symbol again with the same symbol table
-    BlueprintParserCore parser2(0, SourceDataFixture, Blueprint());
-    parser2.symbolTable = parser.symbolTable;
-    Resource resource2;
-    ParseSectionResult result2 = ResourceParser::Parse(markdown.begin(), markdown.end(), rootSection, parser2, resource2);
-    REQUIRE(result2.first.error.code != Error::OK);
+    ResourceModel model;
+    Symbols symbols;
+
+    model.description = "Foo";
+    model.body = "Bar";
+    symbols.push_back(ResourceModelSymbol("Super", model));
+
+    Resource resource;
+    Report report;
+    SectionParserHelper<Resource, ResourceParser>::parse(source, ModelBodySectionType, report, resource, symbols);
+
+    REQUIRE(report.error.code != Error::OK);
 }
