62b83f7552093316616ee037f4fa48b47945684d
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-07-14 22:08:05 +0530

    Finished indentation, symbol and warning unit tests

--- test/test-Warnings.cc
+++ test/test-Warnings.cc
@@ -6,112 +6,92 @@
 //  Copyright (c) 2013 Apiary Inc. All rights reserved.
 //
 
-#include <iterator>
-#include <iostream>
-#include "catch.hpp"
-#include "Fixture.h"
-#include "Parser.h"
+#include "snowcrash.h"
+#include "snowcrashtest.h"
 
 using namespace snowcrash;
 using namespace snowcrashtest;
 
-TEST_CASE("Warn about keywords in API name", "[warnings][#31]")
+TEST_CASE("Warn about keywords in API name", "[warnings]")
 {
-    // Blueprint in question:
-    //R"(
-    //FORMAT: X-1A
-    //
-    //# POST
-    //");
-    const std::string blueprintSource = \
+    mdp::ByteBuffer source = \
     "FORMAT: 1A\n"\
     "\n"\
-    "## POST\n";
-    
-    Parser parser;
-    Result result;
+    "# POST\n";
+
     Blueprint blueprint;
-    parser.parse(blueprintSource, 0, result, blueprint);
-    REQUIRE(result.error.code == Error::OK);
-    // TODO:
-//    REQUIRE(result.warnings.size() == 1);
-//    REQUIRE(result.warnings[0].code == AmbiguityWarning);
-//    
-//    REQUIRE(blueprint.resourceGroups.empty());
+    Report report;
+    parse(source, 0, report, blueprint);
+
+    REQUIRE(report.error.code == Error::OK);
+
+//    REQUIRE(report.warnings.size() == 1);
+//    REQUIRE(report.warnings[0].code == AmbiguityWarning);
+
+//    REQUIRE(blueprint.name.empty());
+    REQUIRE(blueprint.resourceGroups.empty());
 }
 
-TEST_CASE("Warn about brackets in URI template", "[bracketwarnings][issue][#79]")
+TEST_CASE("Warn about brackets in URI template", "[warnings]")
 {
-    // Blueprint in question:
-    //R"(
-    //"FORMAT: 1A\n"\
-    //
-    //# test api
-    //this is a description of the test api
-    //# Group test
-    //A test group
-    //## test [/test/{id}[2]]
-    //"A test uri template
-    //");
-    const std::string blueprintSource = \
-        "FORMAT: 1A\n"\
-        "\n"\
-        "# test api\n"\
-        "this is a description of the test api\n"\
-        "# Group test\n"\
-        "A test group\n"\
-        "## test [/test/{id}[2]]\n"\
-        "A test uri template\n";
+    mdp::ByteBuffer source = \
+    "FORMAT: 1A\n"\
+    "\n"\
+    "# test api\n"\
+    "this is a description of the test api\n"\
+    "# Group test\n"\
+    "A test group\n"\
+    "## test [/test/{id}[2]]\n"\
+    "A test uri template\n";
 
-    Parser parser;
-    Result result;
     Blueprint blueprint;
-    parser.parse(blueprintSource, 0, result, blueprint);
-    REQUIRE(result.warnings.size() == 1);
+    Report report;
+    parse(source, 0, report, blueprint);
+
+    REQUIRE(report.error.code == Error::OK);
+    REQUIRE(report.warnings.size() == 1);
+    REQUIRE(report.warnings[0].code == URIWarning);
 }
 
-TEST_CASE("Warn about unsupported uri template label expansion", "[unsupportedlabelexpansion][issue][#78]")
+TEST_CASE("Warn about unsupported uri template label expansion", "[warnings]")
 {
-    // Blueprint in question:
-    //R"(
-    //"FORMAT: 1A\n"\
-        //
-    //# test api
-    //this is a description of the test api
-    //# Group test
-    //A test group
-    //## test [/test/{.varone}]
-    //"A test uri template
-    //");
-    const std::string blueprintSource = \
-        "FORMAT: 1A\n"\
-        "\n"\
-        "# test api\n"\
-        "this is a description of the test api\n"\
-        "# Group test\n"\
-        "A test group\n"\
-        "## test [/test/{.varone}]\n"\
-        "A test uri template\n";
+    mdp::ByteBuffer source = \
+    "FORMAT: 1A\n"\
+    "\n"\
+    "# test api\n"\
+    "this is a description of the test api\n"\
+    "# Group test\n"\
+    "A test group\n"\
+    "## test [/test/{.varone}]\n"\
+    "A test uri template\n";
 
-    Parser parser;
-    Result result;
     Blueprint blueprint;
-    parser.parse(blueprintSource, 0, result, blueprint);
-    REQUIRE(result.warnings.size() == 1);
+    Report report;
+    parse(source, 0, report, blueprint);
+
+    REQUIRE(report.error.code == Error::OK);
+    REQUIRE(report.warnings.size() == 1);
+    REQUIRE(report.warnings[0].code == URIWarning);
 }
 
-TEST_CASE("Warn about unsupported uri template in abbreviated blueprint","[abbreviatedblueprint][issue][#78]")
+TEST_CASE("Warn about unsupported uri template in abbreviated blueprint", "[warnings]")
 {
-    const std::string blueprintSource = \
-        "FORMAT: 1A\n"\
-        "\n"\
-        "# GET /res/{id}{?a,    b}\n"\
-        "+ Response 200";
+    mdp::ByteBuffer source = \
+    "FORMAT: 1A\n"\
+    "\n"\
+    "# test api\n"\
+    "this is a description of the test api\n"\
+    "# Group test\n"\
+    "A test group\n"\
+    "## GET /res/{id}{?a,    b}\n"\
+    "+ Response 200";
 
-    Parser parser;
-    Result result;
     Blueprint blueprint;
-    parser.parse(blueprintSource, 0, result, blueprint);
-    REQUIRE(result.warnings.size() == 1);
+    Report report;
+    parse(source, 0, report, blueprint);
+
+    REQUIRE(report.error.code == Error::OK);
+    REQUIRE(report.warnings.size() == 1);
+//    REQUIRE(report.warnings[0].code == URIWarning); // the resource '/res/{id}{?a,    b}' is already defined
 }
 
