f1e0835f63cfeeda60d7ca60869f63baa65df221
unknown <al_kh31@yahoo.com>
2014-08-12 19:22:12 +0200

    fix bug on SectionParser.h

--- src/SectionParser.h
+++ src/SectionParser.h
@@ -91,9 +91,10 @@ namespace snowcrash {
                     cur = SectionProcessor<T>::processUnexpectedNode(cur, collection, pd, lastSectionType, report, out);
                 }
                 
-                if (pd.sectionContext() != UndefinedSectionType ||
+                if (cur != collection.end() &&
+                    (pd.sectionContext() != UndefinedSectionType ||
                     (cur->type != mdp::ParagraphMarkdownNodeType &&
-                     cur->type != mdp::CodeMarkdownNodeType)) {
+                     cur->type != mdp::CodeMarkdownNodeType))) {
 
                     lastSectionType = pd.sectionContext();
                 }
