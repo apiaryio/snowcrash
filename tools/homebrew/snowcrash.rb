582f3b6f0788cc151aa07ca18d8366c4abf1acc9
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-10-11 19:20:45 +0200

    Prepare for the v0.15.1 release

--- tools/homebrew/snowcrash.rb
+++ tools/homebrew/snowcrash.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Snowcrash < Formula
   homepage 'http://apiblueprint.org'
-  head 'https://github.com/apiaryio/snowcrash.git', :tag => 'v0.15.0'
+  head 'https://github.com/apiaryio/snowcrash.git', :tag => 'v0.15.1'
 
   def install
     system "./configure"
