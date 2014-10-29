83b79a1e60c8ecfdbb9b3c9a82af7a04125c7b12
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-10-29 13:15:27 +0100

    Added StripMarkdownLink to StringUtility.h

--- test/test-StringUtility.cc
+++ test/test-StringUtility.cc
@@ -13,9 +13,10 @@
 using namespace snowcrash;
 using namespace snowcrashtest;
 
-TEST_CASE("templates for compare equality", "[utility]")
+TEST_CASE("Templates for compare equality", "[utility]")
 {
-    SECTION("testing equality on same types") {
+    SECTION("Testing equality on same types")
+    {
         REQUIRE(IsEqual()(1,1));
         REQUIRE_FALSE(IsEqual()(1,2));
 
@@ -23,8 +24,8 @@ TEST_CASE("templates for compare equality", "[utility]")
         REQUIRE_FALSE(IsEqual()('a','A'));
     }
 
-    SECTION("testing case insensitive equality on same types") {
-
+    SECTION("Testing case insensitive equality on same types")
+    {
         REQUIRE(IsIEqual()('a','a'));
         REQUIRE(IsIEqual()('a','A'));
         REQUIRE(IsIEqual()('A','a'));
@@ -33,15 +34,15 @@ TEST_CASE("templates for compare equality", "[utility]")
         REQUIRE_FALSE(IsIEqual()('a','b'));
     }
 
-    SECTION("both version should not throw while testing diferent types") {
+    SECTION("Both version should not throw while testing diferent types")
+    {
         REQUIRE_NOTHROW(IsEqual()('a',1)); // allow compare diferent types - not throw
         REQUIRE_NOTHROW(IsIEqual()('a',1)); // allow compare diferent types - not throw
     }
-
 }
 
-TEST_CASE("container comparation", "[utility]"){
-
+TEST_CASE("Container comparation", "[utility]")
+{
     REQUIRE(MatchContainers(std::string("abc"), std::string("abc"), IsEqual()));
 
     REQUIRE_FALSE(MatchContainers(std::string("ABC"), std::string("abc"), IsEqual()));
@@ -52,14 +53,19 @@ TEST_CASE("container comparation", "[utility]"){
     REQUIRE_FALSE(MatchContainers(std::string("def"), std::string("ABC"), IsIEqual()));
 }
 
-TEST_CASE("comapare string", "[utility]"){
-
+TEST_CASE("Comapare string", "[utility]")
+{
     REQUIRE(Equal<std::string>()(std::string("abc"), std::string("abc")));
     REQUIRE_FALSE(Equal<std::string>()(std::string("abcd"), std::string("abc")));
 
     REQUIRE(IEqual<std::string>()(std::string("abc"), std::string("ABC")));
     REQUIRE(IEqual<std::string>()(std::string("ABC"), std::string("ABC")));
     REQUIRE_FALSE(IEqual<std::string>()(std::string("abcd"), std::string("abc")));
-
 }
 
+TEST_CASE("Remove markdown link")
+{
+    REQUIRE(StripMarkdownLink("[Google][]") == "Google");
+    REQUIRE(StripMarkdownLink("[Google] [google]") == "Google");
+    REQUIRE(StripMarkdownLink("[Google](https://google.com)") == "Google");
+}
