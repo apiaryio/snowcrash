0a0f6d164ea9516ab16aa7f4d6e82b7f9c4c8f40
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2014-11-14 19:46:33 +0100

    Finished MSONTypeSectionParser tests

--- test/test-MSONTypeSectionParser.cc
+++ test/test-MSONTypeSectionParser.cc
@@ -82,3 +82,228 @@ TEST_CASE("Type Section list block classifier", "[mson][type_section]")
     sectionType = SectionProcessor<mson::TypeSection>::sectionType(markdownAST.children().begin());
     REQUIRE(sectionType == MSONTypeSectionSectionType);
 }
+
+TEST_CASE("Parse canonical mson sample list type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample: 75";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "75");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse array mson sample list type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample: 75, 100";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value.empty());
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().size() == 2);
+    REQUIRE(typeSection.node.content.members().at(0).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values[0].literal == "75");
+    REQUIRE(typeSection.node.content.members().at(1).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values[0].literal == "100");
+}
+
+TEST_CASE("Parse string with comma mson sample list type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample: 75, 100";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "75, 100");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse mson sample list type section with values as list items", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample\n"\
+    "  - red\n"\
+    "  - green\n";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value.empty());
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().size() == 2);
+    REQUIRE(typeSection.node.content.members().at(0).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values[0].literal == "red");
+    REQUIRE(typeSection.node.content.members().at(1).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values[0].literal == "green");
+}
+
+TEST_CASE("Parse multi-line mson sample list type section without newline", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample\n"\
+    "   red\n"\
+    "     green\n\n"\
+    " yellow";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "red\n  green\n\nyellow");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse multi-line mson sample list type section with newline", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample\n\n"\
+    "   red\n"\
+    "     green\n"\
+    "yellow";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "   red\n     green\nyellow");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse markdown multi-line mson sample list type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "- Sample\n"\
+    "  - red\n\n"\
+    "  - green";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "- red\n\n- green");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse mson sample header type section with values as list items", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "# Sample\n"\
+    "  - red\n"\
+    "  - green\n";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value.empty());
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().size() == 2);
+    REQUIRE(typeSection.node.content.members().at(0).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(0).content.value.valueDefinition.values[0].literal == "red");
+    REQUIRE(typeSection.node.content.members().at(1).type == mson::ValueMemberType);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(typeSection.node.content.members().at(1).content.value.valueDefinition.values[0].literal == "green");
+}
+
+TEST_CASE("Parse multi-line mson sample header type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "# Sample\n"\
+    "   red\n"\
+    "     green\n"\
+    "yellow";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "red\n  green");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse multi-line mson sample header type section with multiple nested nodes", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "# Sample\n"\
+    "   red\n"\
+    "     green\n\n"\
+    " yellow";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "red\n  green\nyellow");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
+
+TEST_CASE("Parse markdown multi-line mson sample header type section", "[mson][type_section]")
+{
+    mdp::ByteBuffer source = \
+    "# Sample\n"\
+    "  - red\n\n"\
+    "  - green";
+
+    ParseResult<mson::TypeSection> typeSection;
+    SectionParserHelper<mson::TypeSection, MSONTypeSectionListParser>::parse(source, MSONTypeSectionSectionType, typeSection);
+
+    REQUIRE(typeSection.report.error.code == Error::OK);
+    REQUIRE(typeSection.report.warnings.empty());
+
+    REQUIRE(typeSection.node.type == mson::SampleTypeSectionType);
+    REQUIRE(typeSection.node.content.value == "- red\n\n- green");
+    REQUIRE(typeSection.node.content.description.empty());
+    REQUIRE(typeSection.node.content.members().empty());
+}
--- test/test-MSONValueMemberParser.cc
+++ test/test-MSONValueMemberParser.cc
@@ -56,8 +56,8 @@ TEST_CASE("Parse mson value member with block description", "[mson][value_member
 {
     mdp::ByteBuffer source = \
     "- red (string, required) - A color\n\n"\
-    "  Which is also very nice\n\n"\
-    "  - and awesome\n\n"\
+    "    Which is also very nice\n\n"\
+    "    - and awesome\n\n"\
     "and really really nice\n\n";
 
     ParseResult<mson::ValueMember> valueMember;
@@ -68,21 +68,21 @@ TEST_CASE("Parse mson value member with block description", "[mson][value_member
 
     REQUIRE(valueMember.node.description == "A color");
     REQUIRE(valueMember.node.valueDefinition.values.size() == 1);
+    REQUIRE(valueMember.node.valueDefinition.typeDefinition.attributes == mson::RequiredTypeAttribute);
     REQUIRE(valueMember.node.sections.size() == 1);
     REQUIRE(valueMember.node.sections[0].type == mson::BlockDescriptionTypeSectionType);
     REQUIRE(valueMember.node.sections[0].content.description == "Which is also very nice\n\n- and awesome\n");
-    REQUIRE(valueMember.node.sections[0].content.members().empty());
 }
 
 TEST_CASE("Parse mson value member with block description, default and sample", "[mson][value_member]")
 {
     mdp::ByteBuffer source = \
-    "- red (string, required) - A color\n\n"\
-    "  Which is also very nice\n\n"\
-    "  - and awesome\n\n"\
-    "  - Default: yellow\n"\
-    "  - Sample\n\n"\
-    "    green\n";
+    "- red (string) - A color\n\n"\
+    "    Which is also very nice\n\n"\
+    "    - and awesome\n\n"\
+    "    - Default: yellow\n"\
+    "    - Sample\n\n"\
+    "        green\n";
 
     ParseResult<mson::ValueMember> valueMember;
     SectionParserHelper<mson::ValueMember, MSONValueMemberParser>::parse(source, MSONValueMemberSectionType, valueMember);
@@ -92,16 +92,40 @@ TEST_CASE("Parse mson value member with block description, default and sample", 
 
     REQUIRE(valueMember.node.description == "A color");
     REQUIRE(valueMember.node.valueDefinition.values.size() == 1);
+    REQUIRE(valueMember.node.valueDefinition.values[0].literal == "red");
     REQUIRE(valueMember.node.sections.size() == 3);
     REQUIRE(valueMember.node.sections[0].type == mson::BlockDescriptionTypeSectionType);
     REQUIRE(valueMember.node.sections[0].content.description == "Which is also very nice\n\n- and awesome\n");
-    REQUIRE(valueMember.node.sections[0].content.members().empty());
     REQUIRE(valueMember.node.sections[1].type == mson::DefaultTypeSectionType);
-    REQUIRE(valueMember.node.sections[1].content.description.empty());
     REQUIRE(valueMember.node.sections[1].content.members().size() == 1);
     REQUIRE(valueMember.node.sections[2].type == mson::SampleTypeSectionType);
-    REQUIRE(valueMember.node.sections[2].content.description.empty());
     REQUIRE(valueMember.node.sections[2].content.members().size() == 1);
+}
+
+TEST_CASE("Parse mson value member array with sample", "[mson][value_member]")
+{
+    mdp::ByteBuffer source = \
+    "- (array) - List of colors\n\n"\
+    "    Lots and lots of them\n\n"\
+    "    - Sample\n"\
+    "        - green\n"\
+    "        - red\n"\
+    "        - blue\n"\
+    "        - yellow\n";
+
+    ParseResult<mson::ValueMember> valueMember;
+    SectionParserHelper<mson::ValueMember, MSONValueMemberParser>::parse(source, MSONValueMemberSectionType, valueMember);
+
+    REQUIRE(valueMember.report.error.code == Error::OK);
+    REQUIRE(valueMember.report.warnings.empty());
+
+    REQUIRE(valueMember.node.description == "List of colors");
+    REQUIRE(valueMember.node.valueDefinition.values.empty());
+    REQUIRE(valueMember.node.sections.size() == 3);
+    REQUIRE(valueMember.node.sections[0].type == mson::BlockDescriptionTypeSectionType);
+    REQUIRE(valueMember.node.sections[0].content.description == "Lots and lots of them\n");
+    REQUIRE(valueMember.node.sections[1].type == mson::SampleTypeSectionType);
+    REQUIRE(valueMember.node.sections[1].content.members().size() == 4);
 }
 
 TEST_CASE("Parse mson value member with multiple values", "[mson][value_member]")
@@ -118,4 +142,31 @@ TEST_CASE("Parse mson value member with multiple values", "[mson][value_member]"
     REQUIRE(valueMember.node.description.empty());
     REQUIRE(valueMember.node.sections.empty());
     REQUIRE(valueMember.node.valueDefinition.values.size() == 3);
+}
+
+TEST_CASE("Parse mson value member array with items", "[mson][value_member]")
+{
+    mdp::ByteBuffer source = \
+    "- (array)\n"\
+    "    - red (string) - A sample value\n"\
+    "    - green (string)";
+
+    ParseResult<mson::ValueMember> valueMember;
+    SectionParserHelper<mson::ValueMember, MSONValueMemberParser>::parse(source, MSONValueMemberSectionType, valueMember);
+
+    REQUIRE(valueMember.report.error.code == Error::OK);
+    REQUIRE(valueMember.report.warnings.empty());
+
+    REQUIRE(valueMember.node.description.empty());
+    REQUIRE(valueMember.node.valueDefinition.values.empty());
+    REQUIRE(valueMember.node.valueDefinition.typeDefinition.typeSpecification.name.name == mson::ArrayTypeName);
+    REQUIRE(valueMember.node.sections.size() == 1);
+
+    REQUIRE(valueMember.node.sections[0].type == mson::MemberTypeSectionType);
+    REQUIRE(valueMember.node.sections[0].content.members().size() == 2);
+    REQUIRE(valueMember.node.sections[0].content.members().at(0).type == mson::ValueMemberType);
+    REQUIRE(valueMember.node.sections[0].content.members().at(0).content.value.valueDefinition.values.size() == 1);
+    REQUIRE(valueMember.node.sections[0].content.members().at(0).content.value.description == "A sample value");
+    REQUIRE(valueMember.node.sections[0].content.members().at(1).type == mson::ValueMemberType);
+    REQUIRE(valueMember.node.sections[0].content.members().at(1).content.value.valueDefinition.values.size() == 1);
 }
