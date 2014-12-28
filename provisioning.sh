4521401cc4d5a1e75b2ea373ae5f7153de23d25c
Almad <bugs@almad.net>
2014-12-29 00:52:11 +0100

    Conteinarize snowcrash environment using Docker

--- provisioning.sh
+++ provisioning.sh
@@ -5,7 +5,7 @@ sudo apt-get update -y
 sudo apt-get install -y python-software-properties
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -y
-sudo apt-get install -y gcc-4.7 g++-4.7 gdb build-essential git-core
+sudo apt-get install -y gcc-4.7 g++-4.7 gdb build-essential git-core libgemplugin-ruby
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.6 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7 
 
