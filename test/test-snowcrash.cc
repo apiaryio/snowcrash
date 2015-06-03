8404dc6ed211167eeae489909f8958fa570ef0d8
Kyle Fuller <kyle@fuller.li>
2015-06-03 07:18:33 -0700

    [Line Item] Don't include setext headings

--- test/test-snowcrash.cc
+++ test/test-snowcrash.cc
@@ -726,7 +726,7 @@ TEST_CASE("Don't mess up sourcemaps when there are references", "[parser][#213]"
     REQUIRE(resourceSM.actions.collection[0].method.sourceMap[0].length == 8);
 }
 
-TEST_CASE("doesn't crash while parsing response followed by a block quote", "[parser][#322]")
+TEST_CASE("doesn't crash while parsing response followed by a block quote and heading", "[parser][#322]")
 {
     mdp::ByteBuffer source = \
     "# GET /a\n\n"\
@@ -745,4 +745,26 @@ TEST_CASE("doesn't crash while parsing response followed by a block quote", "[pa
     SourceMap<Action> actionSM = resourceSM.actions.collection[0];
     SourceMap<Payload> payloadSM = actionSM.examples.collection[0].responses.collection[0];
     SourceMapHelper::check(payloadSM.body.sourceMap, 30, 22);
+}
+
+TEST_CASE("doesn't crash while parsing response followed by a block quote settext heading", "[parser][#322]")
+{
+    mdp::ByteBuffer source = \
+    "# GET /a\n\n"\
+    "+ Response 200\n\n"\
+    "        a\n"\
+    ">       b\n"\
+    "        e\n"\
+    "Heading\n"\
+    "-------\n";
+
+    ParseResult<Blueprint> blueprint;
+    parse(source, ExportSourcemapOption, blueprint);
+
+    REQUIRE(blueprint.report.error.code == Error::OK);
+
+    SourceMap<Resource> resourceSM = blueprint.sourceMap.content.elements().collection[0].content.elements().collection[0].content.resource;
+    SourceMap<Action> actionSM = resourceSM.actions.collection[0];
+    SourceMap<Payload> payloadSM = actionSM.examples.collection[0].responses.collection[0];
+    SourceMapHelper::check(payloadSM.body.sourceMap, 30, 22, 1);
 }
