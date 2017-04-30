cc86d90b3b88efd1890a4ad8dfbd51b809af3e96
e.baranov <e.baranov@fun-box.ru>
2017-04-30 19:19:38 +0300

    remove unnecessary changes

--- src/SectionParser.h
+++ src/SectionParser.h
@@ -33,6 +33,7 @@ namespace snowcrash {
                                           const MarkdownNodes& siblings,
                                           SectionParserData& pd,
                                           const ParseResultRef<T>& out) {
+
             SectionLayout layout = DefaultSectionLayout;
             MarkdownNodeIterator cur = Adapter::startingNode(node, pd);
             const MarkdownNodes& collection = Adapter::startingNodeSiblings(node, siblings);
@@ -47,19 +48,20 @@ namespace snowcrash {
                 cur = parseNestedSections(cur, collection, pd, out);
 
                 SectionProcessor<T>::finalize(node, pd, out);
+
                 return Adapter::nextStartingNode(node, siblings, cur);
             }
 
             // Parser redirect layout
             if (layout == RedirectSectionLayout) {
                 SectionProcessor<T>::finalize(node, pd, out);
+
                 return Adapter::nextStartingNode(node, siblings, cur);
             }
 
             // Default layout
-            if (lastCur == cur) {
+            if (lastCur == cur)
                 return Adapter::nextStartingNode(node, siblings, cur);
-            }
 
             // Description nodes
             while(cur != collection.end() &&
@@ -68,9 +70,8 @@ namespace snowcrash {
                 lastCur = cur;
                 cur = SectionProcessor<T>::processDescription(cur, collection, pd, out);
 
-                if (lastCur == cur) {
+                if (lastCur == cur)
                     return Adapter::nextStartingNode(node, siblings, cur);
-                }
             }
 
             // Content nodes
@@ -118,12 +119,10 @@ namespace snowcrash {
                 if (nestedType != UndefinedSectionType) {
                     cur = SectionProcessor<T>::processNestedSection(cur, collection, pd, out);
                 }
-                else {
-                    if (Adapter::nextSkipsUnexpected ||
-                             SectionProcessor<T>::isUnexpectedNode(cur, pd.sectionContext())) {
+                else if (Adapter::nextSkipsUnexpected ||
+                              SectionProcessor<T>::isUnexpectedNode(cur, pd.sectionContext())) {
 
-                        cur = SectionProcessor<T>::processUnexpectedNode(cur, collection, pd, lastSectionType, out);
-                    }
+                    cur = SectionProcessor<T>::processUnexpectedNode(cur, collection, pd, lastSectionType, out);
                 }
 
                 if (cur != collection.end() &&
@@ -136,9 +135,8 @@ namespace snowcrash {
 
                 pd.sectionsContext.pop_back();
 
-                if (lastCur == cur) {
+                if (lastCur == cur)
                     break;
-                }
             }
 
             return cur;
